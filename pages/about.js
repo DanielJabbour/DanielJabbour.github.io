07aa9a631de3ec3727476cfb4a40fe30f212bd6a
danieljabbour <jabbourd@mcmaster.ca>
2018-06-21 23:51:41 -0400

    changed up get to know me page

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,23 @@
 (function(){
   function toggleSection()
   {
-      if(location.hash=="#hashtrap")
-          document.getElementById("portfolioContent").style.display="block";
+      if(location.hash=="#hashtrap"){
+        document.getElementById("portfolioContent").style.display="block";
+        document.getElementById("blogContent").style.display="none";
+      }
+          
+
       else
           document.getElementById("portfolioContent").style.display="none";
+          document.getElementById("blogContent").style.display="block";
   }
+
   if(window.addEventListener)
   {
       window.addEventListener("hashchange",toggleSection,false);
       window.addEventListener("load",toggleSection,false);
   }
+
   else
   {
       window.attachEvent("onhashchange",toggleSection);
